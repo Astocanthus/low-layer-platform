5b5a723140d7b99cb8df41bff92601a34e1e6c9f
Astocanthus <benjifit.night@gmail.com>
2025-10-28 21:15:23 +0100

    add openstack-placement module version

---
+++ terraform/_modules/os_placement/versions.tf
@@ -0,0 +1,18 @@
+# Copyright (C) - LOW-LAYER
+# Contact : contact@low-layer.com
+
+# -----------------------------------------------------------------------------
+# TERRAFORM REQUIREMENTS
+# -----------------------------------------------------------------------------
+# Defines minimum versions and required providers for consistent deployments
+# across different environments and team members
+
+terraform {
+  required_version = ">=1.3.3"
+  required_providers {
+    kubernetes = {
+      source = "hashicorp/kubernetes"
+      version = "2.16.0"
+    }
+  }
+}
